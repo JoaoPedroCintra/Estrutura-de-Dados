39f31c5b0ad327e587fd06670662b8d52a5e20c2
João Pedro Freitas Cintra <joaopedrofc03@hotmail.com>
2023-02-28 10:59:46 -0300

    update

--- exercicios js/aula001/selecaodistinta.js
+++ exercicios js/aula001/selecaodistinta.js
@@ -1,15 +1,16 @@
 let vetor = [ 7,3,1,9,2];
 let menor = 0
-let posicao
+let posicao = 0
 
-    for (let i = 0; i < vetor.length; i++) {
+
+   for (let i = 0; i < vetor.length; i++) {
         menor = vetor[i]
         for (let j = 0; j < vetor.length; j++) {
             if(menor > vetor[j]){
                 menor = vetor[j]
                 posicao = j
             }
-        }                                                   //ta errado
+        }                                                  
         vetor[posicao] = vetor[i]
         vetor[i] = menor
 
