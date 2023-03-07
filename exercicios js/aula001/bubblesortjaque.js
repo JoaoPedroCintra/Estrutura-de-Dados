6c09a1a2f32aa6e68e71cff7161626d5d378b4fa
João Pedro Freitas Cintra <joaopedrofc03@hotmail.com>
2023-03-07 10:47:52 -0300

    update

--- exercicios js/aula001/bubblesortjaque.js
+++ exercicios js/aula001/bubblesortjaque.js
@@ -1,7 +1,5 @@
-function bubblesortjaque() {
-let vetor = [5, 4, 3, 2, 1]
+function bubblesortjaque(vetor) {
 let aux = 0
-
 for (let j = 0; j < vetor.length-1;j++) {
     for (let i = 0; i < vetor.length-j-1; i++) {
         if (vetor[i] > vetor[i + 1]) {
@@ -14,7 +12,10 @@ for (let j = 0; j < vetor.length-1;j++) {
 
 }
 
+let vetor = [5, 4, 3, 2, 1]
+bubblesortjaque(vetor)
+console.log(vetor)
 
 console.time("bubblesort...")
-bubblesortjaque()                                              
+bubblesortjaque(vetor)                                              
 console.timeEnd("bubblesort...")
