f9e308707cc944ddf434b02a58742796becbf12c
JoaoPedroCintra <joaopedrofc03@hotmail.com>
2023-03-28 11:04:36 -0300

    Update atividade.js

--- exercicios js/Trabalho/atividade.js
+++ exercicios js/Trabalho/atividade.js
@@ -55,6 +55,36 @@ function insercaoDireta(vetor) {
     return vetor
 }
 
+function quicksort(vetor,esquerda,direita) {
+    var i = esquerda
+    var j = direita
+    var aux
+    var pivotidx = (esquerda + direita)/2
+    var pivot = parseInt(vetor[pivotidx.toFixed()])
+    //partição
+    while(i <= j){
+        while(parseInt(vetor[i]) < pivot)
+            i++
+        while(parseInt(vetor[j]) > pivot)
+            j--
+        if( i <= j){
+            aux = vetor[i]
+            vetor[i] = vetor[j]
+            vetor[j] = aux
+            i++
+            j--
+        }
+    }   
+
+    //Recursão
+    if(esquerda < j )
+        quicksort(vetor,esquerda,j)
+    if ( i < direita )
+        quicksort(vetor,i,direita)
+    return vetor
+
+}   
+
 
 
 let vetor_ordenado = []
@@ -183,3 +213,4 @@ insercaoDireta(insercao_desordenado)
 console.timeEnd("vetor desordenado...")
 
 console.log("\n")
+
