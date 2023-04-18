737323ac2d710af4105ce95704376b8969847551
João Pedro Freitas Cintra <joaopedrofc03@hotmail.com>
2023-04-18 11:07:13 -0300

    up

--- exercicios js/aula006/exe003.js
+++ exercicios js/aula006/exe003.js
@@ -1,33 +1,32 @@
 //Fazer uma função que verifique se uma dada expressão aritmética possui ou não parênteses, colchetes e chaves corretamente utilizados, tanto número quanto posição
 //usando pilha
 var pilha = []
+var vetor = []
+let cont = 0
 
-expressao = "{()}[]"
-pilha = expressao.split("")
+expressao = "({1+2)5}[6+7]"
+vetor = expressao.split("")
+
+console.log(vetor)
+
+for (var i = 0; i < vetor.length; i++) {
+
+    if (vetor[i] == "{" || vetor[i] == "(" || vetor[i] == "[" ) {
+        pilha.push(vetor[i])
+        cont++
+    }
+    
+   if (vetor[i]=="}" && pilha[cont-1] == "{") {
+        pilha.pop()
+    }
+    else if(vetor[i]=="]" && pilha[cont-1] == "["){
+        pilha.pop()
+
+    }
+    else if(vetor[i]==")" && pilha[cont-1] == "("){
+        pilha.pop()
+    }
+
+}
 
 console.log(pilha)
-var vetor = []
-
-for (var i=0;i<pilha.length;i++)
-{
-    if(pilha[i] == "(" || pilha[i] == "[" || pilha[i] == "{" || pilha[i] == ")" || pilha[i] == "]" || pilha[i] == "}")
-    {
-        vetor.push(pilha[i])
-    }
-}
-
-console.log(vetor)
-var vetor2 = []
-i = 0
-while(vetor[i] == "(" || vetor[i] == "[" || vetor[i] == "{")
-{
-    vetor2.push(vetor[i])
-    i++
-}
-
-vetor2.length * vetor.shift()
-
-
-
-console.log(vetor2)
-console.log(vetor)
