78de50d06e177e0ea1d5e6f2226f8e1118dfccef
JoaoPedroCintra <joaopedrofc03@hotmail.com>
2023-04-24 19:29:29 -0300

    Create exe.js

---
+++ Exercicios-js/Trabalho_2ºbim/exe.js
@@ -0,0 +1,38 @@
+var pilha = []
+var vetor = []
+let indice = 0
+let expressao = "{(1+2)5} - [6+7]"
+
+vetor = expressao.split("")
+console.log(vetor)
+
+for (var i = 0; i < vetor.length; i++) {
+    
+    if (vetor[i] == "{" || vetor[i] == "(" || vetor[i] == "[") {
+        pilha.push(vetor[i])
+    }
+    else if (vetor[i] == "}" || vetor[i] == ")" || vetor[i] == "]") {
+        pilha.push(vetor[i])
+
+        indice = pilha.length - 1
+
+        if (pilha[indice] == "}" && pilha[indice - 1] == "{") {
+            pilha.splice((indice - 1), 2)
+        }
+        else if (pilha[indice] == "]" && pilha[indice - 1] == "[") {
+            pilha.splice((indice - 1), 2)
+        }
+        else if (pilha[indice] == ")" && pilha[indice - 1] == "(") {
+            pilha.splice((indice - 1), 2)
+        }
+    }
+}
+
+if(pilha.length == 0){
+    console.log("A Expressão está correta!! \npilha vazia :")
+    console.log(pilha)
+}
+else {
+    console.log("Erro! A pilha não está vazia:")
+    console.log(pilha)
+}
