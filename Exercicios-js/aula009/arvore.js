6bb6ec2b42672c422e6e91e00d5b40ecc92ce769
João Pedro Freitas Cintra <joaopedrofc03@hotmail.com>
2023-05-09 10:19:03 -0300

    Create arvore.js

---
+++ Exercicios-js/aula009/arvore.js
@@ -0,0 +1,13 @@
+/*
+Estrutura de dados n linear, utilizaxa para rápido acesso aos dados
+nó especial  -> raiz 
+nó sem filhos -> folha/terminal
+nó com filhos -> nós internos, n-folha, interior
+grau de um nó : nº de filhos diretos
+nível: de acordo com a linha na arvore
+profundidade/altura: nivel maximo de qualquer folha na arvore 
+
+Arvore Ordenada
+
+Arvores Binárias  -> arvore ordenada de grau 2
+*/
